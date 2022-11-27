c3b057fcd27e2dbd2328adea9ee7a1a34fb85c46
iamvugarr <ilqar.qurbanov2007@gmail.com>
2022-11-27 20:13:23 +0400

    add task1

---
+++ JS tasklar/JS1/app.js
@@ -0,0 +1,21 @@
+// let uzunluq = prompt('duzbucaqlinin uzunlugunu daxil edin: ')
+// let en = prompt('duzbucaqlinin enini daxil edin: ')
+
+// let sahe= uzunluq * en
+
+// console.log('duzbucaqlinin sahesi= ' +sahe)
+
+
+
+
+
+// let dogumili = prompt('dogum ilinizi daxil edin: ')
+// let yas= 2022 - dogumili
+// console.log('yas=' +yas)
+
+
+
+// let km= prompt('km-i daxil edin: ')
+// let milDeyeri = 0.6214
+// alert(km * milDeyeri)
+// console.log(km * milDeyeri)
