c4f23cc930904659fde8c6946ccd3c481f6c1831
iamvugarr <ilqar.qurbanov2007@gmail.com>
2022-11-27 20:20:45 +0400

    add task2

---
+++ JS tasklar/JS2/app.js
@@ -0,0 +1,44 @@
+//----------------------------------------EDEDIN KVADRAT KOKU--------------------------
+// let eded= +prompt('ededi daxil edin= ')
+// let kvadratkok= eded ** (1/2)
+
+// console.log('ededin kavdrat koku= ' +kvadratkok)
+
+//--------------------------------SELSI FARENHEYT CEVIREN----------------------------
+// let selsideyeri= +prompt('selsi deyerini daxil edin: ')
+// let farenheytdeyeri= selsideyeri * 1.8 + 32
+
+// console.log('farenheyt deyeri= ' +farenheytdeyeri)
+
+//--------------------------------EDEDIN MENFI MUSBET 0 OLDUGUNU TAPIR--------------------------------
+// let eded= +prompt('ededi daxil edin: ')
+
+// if(eded>0){
+//     console.log('eded musbetdir ')
+// }
+
+// else if(eded<0){
+//     console.log('eded menfidir')
+// }
+
+// else{
+//     console.log('eded 0-dir')
+// }
+
+//-----------------------------EDEDIN TEK VE YA CUT OLDUGUNU TAPIR---------------------------
+// let eded= +prompt('ededi daxil edin: ')
+
+// if(eded%2==0){
+//     console.log('eded cutdur')
+// }
+
+// else{
+//     console.log('eded tekdir')
+// }
+
+let eded1= 20
+let eded2= 49
+let eded3= 130
+
+let c= Math.max(eded1, eded2, eded3)
+console.log(c)
