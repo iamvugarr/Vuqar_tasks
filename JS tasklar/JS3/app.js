178865e42124aab272bcb6e558e0110ee18cd03e
iamvugarr <ilqar.qurbanov2007@gmail.com>
2022-11-29 22:32:07 +0400

    add task 3

---
+++ JS tasklar/JS3/app.js
@@ -0,0 +1,29 @@
+// 1. logGreeting() adında funksiya tərtib edin hansıki konsola «[Adınız] is a JS developer» mesajı çıxarsın.
+
+
+// function logGreeting(adiniz){
+//     return console.log(`${adiniz} is a JS developer`)
+// }
+
+// logGreeting('Vuqar')
+
+
+// 2. İstifadəçi adını parametr kimi qəbul edən və konsola «[İstifadəçi adı] is active now» mesajı çıxaran funksiya tərtib edin.
+// Funksiyanı 3 müxtəlif ad üçün çağırın.
+
+// function logGreeting(istifadeciAdi){
+//     return console.log(`${istifadeciAdi} is active now.`)
+// }
+
+// logGreeting('Vuqar')
+// logGreeting('Ilqar')
+// logGreeting('Elsen')
+
+
+// 3. Parametr kimi ədədi qəbul edən və onu dördüncü dərəcə qüvvətə yülsəldərək konsolda göstərən fourth() funksiyasını yazın.
+
+// function fourth(eded){
+//     return Math.pow(eded, 4)
+// }
+
+// console.log(`${fourth(15)}`)
