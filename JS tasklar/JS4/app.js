566e828d2d0e431541def6689939b1b9f7c18921
iamvugarr <ilqar.qurbanov2007@gmail.com>
2022-12-03 22:29:01 +0400

    add task 4

---
+++ JS tasklar/JS4/app.js
@@ -0,0 +1,26 @@
+// 4. Sonu 0 ilə bitən bütün üçrəqəmli ədədləri konsola çıxaran proqram yazın. (for ile yazin)
+
+// for(let i =100; i<=990; i= i+10){
+//     console.log(i)
+//  }
+
+// 5. Bütün ikirəqəmli tək ədədlərin cəmini konsola çıxaran proqram yazın.
+
+// let cem = 0
+// for(let i=11; i<=99; i= i+2){
+//     cem+=i
+// }
+// console.log(`butun 2 reqemli tek ededlerin cemi: ${cem}`)
+
+// 6. İstifadəçinin daxil etdiyi ədədin bölünəni olan bütün üçrəqəmli ədədlərin cəmini hesablayan proqram yazın. Meselen, 50-ye hansi ucreqemli ededler bolunur?: 100, 150, 200, 250, ....
+
+// let eded = +prompt('ededi daxil edin: ')
+// let sum = 0
+
+// for(let i = 100 ; i < 1000 ; i++){
+//     if(i % eded === 0){
+//         sum +=i
+//     }
+// }
+
+// console.log(sum)
