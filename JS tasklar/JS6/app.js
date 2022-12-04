8132e5feffa26f77def94e9b3923bc73bebb4221
iamvugarr <ilqar.qurbanov2007@gmail.com>
2022-12-04 15:25:40 +0400

    add task 6

---
+++ JS tasklar/JS6/app.js
@@ -0,0 +1,28 @@
+// Dersde yaratdigimiz User constructor-una oxsar, Car constructor-u yaradin. Bu field-lari olsun: model, year, company, drive() , owner. 
+
+//  function Car(model){ 
+// this.model=model 
+// }
+
+// const car = new Car('rav4',1997, 'toyota', 'ravan');
+
+// car.drive() // ravan is driving rav4.
+
+// function car(model, year, company, owner){
+//     this.model=model,
+//     this.year=year,
+//     this.company=company,
+//     this.owner=owner,
+//     this.drive= () => {
+//         console.log(`${owner} is driving ${model}`)
+//     }
+// }
+
+// const car1= new car('F1 W11', 2020, 'Mercedes', 'Lewis Hamilton')
+// const car2= new car('F1 RB18', 2022, 'Red Bull F1', 'Max Verstappen')
+
+// console.log(car1)
+// console.log(car2)
+
+// car1.drive()
+// car2.drive()
