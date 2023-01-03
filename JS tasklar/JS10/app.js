cb3e579d099518adcdab8c87c8a03da86669976b
iamvugarr <ilqar.qurbanov2007@gmail.com>
2023-01-03 17:11:48 +0400

    add task 10

---
+++ JS tasklar/JS10/app.js
@@ -0,0 +1,33 @@
+// 1. Massiv yaradin.  olcusunu 5e menimsedin ve prompt-la massivi number-lerle doldurun.
+
+// let eded1 = +prompt('birinci ededi daxil edin: ')
+// let eded2 = +prompt('ikinci ededi daxil edin: ')
+// let eded3 = +prompt('ucuncu ededi daxil edin: ')
+// let eded4 = +prompt('dorduncu ededi daxil edin: ')
+
+// const arr = [ eded1, eded2, eded3, eded4 ]
+// console.log(arr)
+// arr.length = 5
+
+//------------------------------------------------------------------------------------------------
+
+// 2. Funksiya yazin ki, massivde olan butun elementlerin hasilini ekrana cixarsin.
+
+// let numbers = [1,2,3,4,5,6,7,8,9,10]
+// let hasil = numbers.reduce((a,b) => a*b)
+// console.log(hasil)
+
+//------------------------------------------------------------------------------------------------
+
+// 3. multiply([a,b,c]) funksiyasi yaradin. Array
+//  qebul etsin. ve onlarin cemini ekrana cixarsin.
+
+// const cem = (arr) => {
+//     let sum = 0
+//     arr.forEach((elem) =>{
+//         sum += elem
+//     })
+//     return sum
+// }
+
+// console.log(cem([1,2,3,4,5]))
